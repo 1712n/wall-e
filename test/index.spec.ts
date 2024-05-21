d62e32e31f847fb76c5493edead8dac036dc0c71
jalmonter <116984708+jalmonter@users.noreply.github.com>
2024-05-21 14:44:57 -0400

    chore: initialize wrangler project

---
+++ test/index.spec.ts
@@ -0,0 +1,15 @@
+import { SELF } from 'cloudflare:test';
+import { describe, it, expect } from 'vitest';
+
+import '../src/index';
+
+const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+
+describe('GitHub App', () => {
+	it('responds with "Nothing to see here..."', async () => {
+		const request = new IncomingRequest('https://example.com');
+		const response = await SELF.fetch(request);
+		const text = await response.text();
+		expect(text).toBe('<h1>Nothing to see here...</h1>');
+	});
+});
