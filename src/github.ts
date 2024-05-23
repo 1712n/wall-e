432870b5c1409733c4f8db12ec70c3cfe77c4013
jalmonter <116984708+jalmonter@users.noreply.github.com>
2024-05-23 01:10:19 -0400

    refactor(wip): github event handler

---
+++ src/github.ts
@@ -0,0 +1,140 @@
+import { App } from '@octokit/app';
+import { Octokit } from '@octokit/core';
+import { EmitterWebhookEvent } from '@octokit/webhooks';
+
+type UserCommand = {
+	name: string;
+	args?: string[];
+};
+
+type InvokedActionHandler = (event: EmitterWebhookEvent<'issue_comment'>, command: UserCommand) => Promise<void>;
+
+type VerifyRequestParams = {
+	headers: Headers;
+	payload: any;
+};
+
+type GitHubOptions = {
+	appId: string;
+	privateKey: string;
+	installationId: number;
+	webhooks: {
+		secret: string;
+	};
+};
+
+export class GitHub {
+	private app: App;
+	private ready: boolean = false;
+	private octokit: Promise<Octokit>;
+
+	constructor({ appId, privateKey, installationId, webhooks }: GitHubOptions) {
+		this.app = new App({
+			appId: appId,
+			privateKey: privateKey,
+			webhooks,
+		});
+		this.octokit = this.app.getInstallationOctokit(installationId);
+	}
+
+	public async setup(handler: InvokedActionHandler): Promise<void> {
+		if (this.ready) {
+			throw new Error('GitHub is already setup');
+		}
+
+		this.app.webhooks.on('issue_comment', async (event) => {
+			const { action, comment } = event.payload;
+			if (!comment.user) {
+				return;
+			}
+
+			if (comment.user.type !== 'User') {
+				return;
+			}
+
+			if (action === 'deleted') {
+				return;
+			}
+
+			const ignore = !comment.body.startsWith('/wall-e');
+			if (ignore) {
+				return;
+			}
+
+			const parsed = comment.body.split(' ');
+			if (parsed.length < 2) {
+				await this.postComment(event, 'Invalid command');
+			}
+
+			const [_, name, ...args] = parsed;
+
+			const command = {
+				name: name.replace('/', ''),
+				args,
+			};
+			await handler(event, command);
+		});
+
+		this.ready = true;
+	}
+
+	public async verifyRequest({ headers, payload }: VerifyRequestParams): Promise<Response> {
+		const id = headers.get('x-github-delivery');
+		const name = headers.get('x-github-event') as any;
+		const signature = headers.get('x-hub-signature-256');
+
+		if (!id) {
+			throw new Error('Missing x-github-delivery header');
+		}
+
+		if (!name) {
+			throw new Error('Missing x-github-event header');
+		}
+
+		if (!signature) {
+			throw new Error('Missing x-hub-signature-256 header');
+		}
+
+		await this.app.webhooks.verifyAndReceive({
+			id: id,
+			name: name,
+			signature: signature,
+			payload: JSON.stringify(payload),
+		});
+
+		const response = JSON.stringify({
+			ok: true,
+		});
+		return new Response(response, {
+			headers: {
+				'content-type': 'application/json',
+			},
+		});
+	}
+
+	public async postComment(event: EmitterWebhookEvent<'issue_comment'>, body: string) {
+		const octokit = await this.octokit;
+		const comment = await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+			owner: event.payload.repository.owner.login,
+			repo: event.payload.repository.name,
+			issue_number: event.payload.issue.number,
+			body: body,
+		});
+
+		if (comment.status !== 201) {
+			this.app.log.error('Failed to send comment', event);
+			return false;
+		}
+
+		return true;
+	}
+
+	public async getRepoContents(event: EmitterWebhookEvent<'issue_comment'>, path: string) {
+		const octokit = await this.octokit;
+		return octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+			owner: event.payload.repository.owner.login,
+			repo: event.payload.repository.name,
+			path: path,
+		});
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,53 @@
-import { App } from '@octokit/app';
+import { GitHub } from './github';
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		switch (request.method) {
-			case 'GET':
+		try {
+			if (request.method === 'GET') {
 				return new Response('<h2>Nothing to see here...</h2>', {
 					headers: { 'content-type': 'text/html' },
 				});
-		}
+			}
 
-		const id = request.headers.get('x-github-delivery');
-		const name = request.headers.get('x-github-event') as any;
-		const signature = request.headers.get('x-hub-signature-256');
+			const payload = await request.json<any>();
 
-		if (!id || !name || !signature) {
+			const github = new GitHub({
+				appId: env.GH_APP_ID,
+				privateKey: env.GH_PRIVATE_KEY,
+				installationId: payload.installation.id,
+				webhooks: {
+					secret: env.GH_WEBHOOK_SECRET,
+				},
+			});
+
+			await github.setup(async (event, command) => {
+				switch (command.name) {
+					case 'hello':
+						{
+							const body = 'Hello from Wall-E!';
+							await github.postComment(event, body);
+						}
+						break;
+
+					case 'help':
+						{
+							const body = 'Available commands: `/wall-e hello`';
+							await github.postComment(event, body);
+						}
+						break;
+				}
+			});
+
+			return github.verifyRequest({ headers: request.headers, payload });
+		} catch (error) {
+			console.error(error);
+
 			const response = JSON.stringify({
-				error: 'Missing required headers',
+				error: 'An error occurred while processing the request. Check the logs for more information.',
 				ok: false,
 			});
 			return new Response(response, {
-				status: 400,
-				headers: {
-					'content-type': 'application/json',
-				},
-			});
-		}
-
-		const payload = await request.json<any>();
-		const installationId = payload.installation.id;
-
-		const app = new App({
-			appId: env.GH_APP_ID,
-			privateKey: env.GH_PRIVATE_KEY,
-			webhooks: {
-				secret: env.GH_WEBHOOK_SECRET,
-			},
-		});
-
-		app.webhooks.on('issue_comment', async (event) => {
-			const { action, comment, issue, repository } = event.payload;
-			if (!comment.user) {
-				return;
-			}
-
-			if (comment.user.type !== 'User') {
-				return;
-			}
-
-			if (action === 'deleted') {
-				return;
-			}
-
-			const isInvoked = comment.body.startsWith('/walle') === true;
-			if (!isInvoked) {
-				return;
-			}
-
-			const octokit = await app.getInstallationOctokit(installationId);
-			const sent = await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
-				owner: repository.owner.login,
-				repo: repository.name,
-				issue_number: issue.number,
-				body: `Hello @${comment.user.login}!`,
-			});
-
-			if (sent.status !== 201) {
-				app.log.error('Failed to send comment', sent);
-			}
-		});
-
-		try {
-			await app.webhooks.verifyAndReceive({
-				id: id,
-				name: name,
-				signature: signature,
-				payload: JSON.stringify(payload),
-			});
-
-			const response = JSON.stringify({
-				ok: true,
-			});
-			return new Response(response, {
-				headers: {
-					'content-type': 'application/json',
-				},
-			});
-		} catch (error: any) {
-			app.log.error(error.message);
-
-			const response = JSON.stringify({
-				error: `Unable to verify and receive request: ${error.message}`,
-				ok: false,
-			});
-			return new Response(response, {
-				status: 400,
+				status: 500,
 				headers: {
 					'content-type': 'application/json',
 				},
