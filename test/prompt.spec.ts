3b8858c11de6110ce28183612c1ec1378c2fbd35
jalmonter <116984708+jalmonter@users.noreply.github.com>
2024-06-05 13:37:46 -0400

    test: prompt builder

---
+++ test/prompt.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+
+import { buildPrompt } from '../src/prompt';
+
+describe('Prompt builder', () => {
+  it('returns a string containig the instructions, Test file and Cloudflare Documentation', async () => {
+    const testFile = '{{ test file goes here }}';
+    const prompt = buildPrompt({ testFile });
+    expect(prompt).toContain(testFile);
+  });
+});
