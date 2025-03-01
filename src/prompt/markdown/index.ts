c904dc0e292e876b1cf00d88a1b632583e0b8317
sibbox <sibbox@gmail.com>
2025-03-01 16:19:03 +0000

    adding js code gen functionality

--- src/prompt/markdown/index.ts
+++ src/prompt/markdown/index.ts
@@ -1,6 +1,13 @@
-import generateWorker from './generate_worker.md';
+import generateWorkerTs from './generate_worker_ts.md';
+import generateWorkerJs from './generate_worker_js.md';
 import improveWorker from './improve_worker.md';
 import analyzeSpecFile from './analyze_spec_file.md';
 import specFileBestPractices from '../../../wiki/spec_file_best_practices.md';
 
-export { generateWorker, improveWorker, analyzeSpecFile, specFileBestPractices };
+export {
+  generateWorkerTs,
+  generateWorkerJs,
+  improveWorker,
+  analyzeSpecFile,
+  specFileBestPractices
+};
