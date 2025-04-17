06ab3f1d349d303af419c295677dee2091434997
Arina Razmyslovich <55647212+Lavriz@users.noreply.github.com>
2025-04-17 17:56:25 +0300

    fix: put temp back

--- src/providers/openai.ts
+++ src/providers/openai.ts
@@ -37,6 +37,11 @@ export function openAiRequest({ model, apiKey, prompts, temperature, stream }: P
 	const query: OpenAIQuery = {
 		model,
 		stream,
+		/**
+		 * What sampling temperature to use, between 0 and 2.
+		 * Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
+		 * https://platform.openai.com/docs/api-reference/chat/create#chat-create-temperature
+		 */
 		temperature: temperature * 2,
 		seed: 0,
 	};
